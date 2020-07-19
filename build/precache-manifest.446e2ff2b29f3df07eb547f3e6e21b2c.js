cd36e7b1b37e52784db7eb1563b5245aaa96ea9f
neutralboy <leopoorna99@gmail.com>
2020-07-19 14:10:10 +0530

    with build

---
+++ build/precache-manifest.446e2ff2b29f3df07eb547f3e6e21b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4280a1589938d56366e527edc91e7f41",
+    "url": "/typescript-quiz-app/index.html"
+  },
+  {
+    "revision": "75cbf06be25dd19c5f94",
+    "url": "/typescript-quiz-app/static/css/2.69429974.chunk.css"
+  },
+  {
+    "revision": "f973f97ffbf9b8a3151b",
+    "url": "/typescript-quiz-app/static/css/main.9757329f.chunk.css"
+  },
+  {
+    "revision": "75cbf06be25dd19c5f94",
+    "url": "/typescript-quiz-app/static/js/2.e915cfb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typescript-quiz-app/static/js/2.e915cfb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f973f97ffbf9b8a3151b",
+    "url": "/typescript-quiz-app/static/js/main.a902afc6.chunk.js"
+  },
+  {
+    "revision": "82a116cf9c808328b544",
+    "url": "/typescript-quiz-app/static/js/runtime-main.4640ace8.js"
+  }
+]);
